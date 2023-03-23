4caadfbdc53dd099b9429b88f4e822cf8ca82d56
nnodim <nnodimobinna@gmail.com>
2023-03-23 04:28:53 +0000

    correction to task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,13 +7,3 @@ export default function getFullResponseFromAPI(success) {
     });
   });
 }
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (!success) reject(new Error('The fake API is not working currently'));
-    resolve({
-      status: 200,
-      body: 'Success',
-    });
-  });
-}
-
