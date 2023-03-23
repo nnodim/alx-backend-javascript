f68680e2e02d5592981e0bf8e2fb8cdb36edc982
nnodim <nnodimobinna@gmail.com>
2023-03-23 04:16:37 +0000

    task 1. Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,19 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (!success) reject(new Error('The fake API is not working currently'));
+    resolve({
+      status: 200,
+      body: 'Success',
+    });
+  });
+}
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (!success) reject(new Error('The fake API is not working currently'));
+    resolve({
+      status: 200,
+      body: 'Success',
+    });
+  });
+}
+
